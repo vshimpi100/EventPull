c8e7aa490fa15bec8759dc89b029b0a145dd4658
Vivek Shimpi <vshimpi100@gmail.com>
2019-04-18 18:03:23 -0400

    API connected and running properly

---
+++ client/build/precache-manifest.ff4abffb926bdbea11d9c85e8fdb1ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7199bc740d1e62b9c1e",
+    "url": "/static/js/main.6700ebc5.chunk.js"
+  },
+  {
+    "revision": "1b77fb388d955aca120b",
+    "url": "/static/js/2.0a65730a.chunk.js"
+  },
+  {
+    "revision": "a7199bc740d1e62b9c1e",
+    "url": "/static/css/main.7d201bfa.chunk.css"
+  },
+  {
+    "revision": "1fe570620fc4749724beaa241b1bfbf6",
+    "url": "/index.html"
+  }
+];
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,16 @@ const userRoutes = require("./users");
 const nicheRoutes = require("./niches");
 const eventRoutes = require("./events");
 const commentRoutes = require("./comments");
+const docsRoutes = require("./docs");
 
 // API routes
+router.use("/docs", docsRoutes);
 router.use("/e", eventRoutes);
 router.use("/n", nicheRoutes);
 router.use("/u", userRoutes);
 router.use("/c", commentRoutes);
+router.use("/", function(req, res) {
+  res.send("Hello World!");
+});
 
 module.exports = router;
