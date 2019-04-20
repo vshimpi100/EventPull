2086dac986ff36ad24b17217a3efc558647a43a1
Zach <95zmartin@gmail.com>
2019-04-20 13:50:13 -0400

    added desktop navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Nav from './components/shared/layouts/Nav';
-import Explore from './pages/explore';
+import DesktopNav from './components/desktop/shared/layouts/Nav';
+import MobileNav from './components/mobile/shared/layouts/Nav';
+import "antd/dist/antd.css";
+import "antd-mobile/dist/antd-mobile.css";
+import windowSize from 'react-window-size';
+// import Explore from './pages/explore';
 
 class App extends Component {
+
+    handleView = (width) => {
+        console.log(width);
+        if (width <= 1024) {
+            return (
+                <MobileNav />
+            )
+        } else {
+            return (
+                <DesktopNav />
+            )
+        }
+    }
+
     render() {
         return (
             <Router>
                 <div>
-                    <Nav />
-                    <Route exact path='/' component={Explore} />
+                    {this.handleView(this.props.windowWidth)}
+                    {/* <Route exact path='/' component={Explore} /> */}
                 </div>
             </Router>
         )
     }
 }
 
-export default App;
+export default windowSize(App);
