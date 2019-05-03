7286d23c97dd06e8d6dde0bcd00cabe9365d152b
Dhruv Patel <darup67@gmail.com>
2019-05-02 21:03:22 -0400

    eventpull

--- client/src/utils/stripecharge.js
+++ client/src/utils/stripecharge.js
@@ -1,13 +1,15 @@
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 var stripe = require("stripe")("pk_test_qZyMF8Y7CJanasgzsG4U0OqM00YJXKuE18");
+var elements = stripe.elements();
+
 
 stripe.charges.create({
   amount: 1000,
   currency: "usd",
   source: "tok_visa",
   transfer_data: {
-    destination: "{CONNECTED_STRIPE_ACCOUNT_ID}",
+    destination: "{darup67}",
   },
 }).then(function(charge) {
   // asynchronously called
