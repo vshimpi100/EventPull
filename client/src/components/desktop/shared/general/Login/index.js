d7d0e9023b2bce5fa7d84bf0ec73db51e0564e60
Vivek Shimpi <vshimpi100@gmail.com>
2019-05-04 11:27:36 -0400

    Adding confirmation modal

--- client/src/components/desktop/shared/general/Login/index.js
+++ client/src/components/desktop/shared/general/Login/index.js
@@ -8,36 +8,59 @@ class Login extends Component {
     username: null,
     password: null,
     email: null,
+    validationCode: null,
     loading: false
   };
 
   handleFormSubmit = e => {
     e.preventDefault();
     if (!this.state.email) {
-      auth.signIn(this.state.username, this.state.password)
-        .then(res => {
-          console.log(res, "this is on the frontend");
-          this.props.handleLoginSubmit(res);
-          this.props.handleCancel();
-          this.setState({
-            username: "",
-            password: "",
-            email: "",
-          });
-        })
-    } else {
-      auth.signUp(this.state.username, this.state.password, this.state.email)
+      auth.signIn(this.state.username, this.state.password).then(res => {
+        console.log(res, "this is on the frontend");
+        console.log("validation status", this.props.isPendingValidation);
+        this.props.handleLoginSubmit(res);
+        this.props.handleCancel();
+        this.setState({
+          username: "",
+          password: "",
+          email: ""
+        });
+      });
+    } else if (!this.state.validationCode) {
+      auth
+        .signUp(this.state.username, this.state.password, this.state.email)
         .then(res => {
           console.log(res, "New user created");
+          this.props.handleValidationPending();
+        });
+    } else {
+      auth
+        .confirmSignUp(this.state.username, this.state.code)
+        .then(res => {
+          auth.signIn(this.state.username, this.state.password).then(res => {
+            this.props.handleLoginSubmit(res);
+            this.props.handleCancel();
+            this.setState({
+              username: "",
+              password: "",
+              email: ""
+            });
+          });
+          this.props.handleValidationPending();
         })
+        .catch(err => console.log(err));
     }
-    
+  };
 
+  resendValidation = () => {
+    auth
+      .resendConfirmation(this.state.username)
+      .then(console.log("email sent"))
+      .catch(err => console.log(err));
   };
 
   handleInputChange = e => {
     const { name, value } = e.target;
-
     this.setState({
       [name]: value
     });
@@ -59,7 +82,13 @@ class Login extends Component {
           <Col span={6} className="Art" />
           <Col span={16} className="login-content-body">
             <Row>
-              <h1 className="form-title">{this.props.isNewUser ? "Sign Up" : "Log In"}</h1>
+              <h1 className="form-title">
+                {!this.props.isPendingValidation
+                  ? this.props.isNewUser
+                    ? "Sign Up"
+                    : "Log In"
+                  : "Enter Validation Code"}
+              </h1>
             </Row>
             <Row>
               <Form
@@ -76,24 +105,39 @@ class Login extends Component {
                     id="username"
                   />
                 </Form.Item>
-                <Form.Item type="text">
+                {!this.props.isPendingValidation ? (
+                  <div>
+                    <Form.Item type="text">
+                      <Input
+                        value={this.state.password}
+                        onChange={this.handleInputChange}
+                        name="password"
+                        placeholder="Password"
+                        id="password"
+                      />
+                    </Form.Item>
+                    {this.props.isNewUser ? (
+                      <Form.Item type="text">
+                        <Input
+                          value={this.state.email}
+                          onChange={this.handleInputChange}
+                          name="email"
+                          placeholder="Email"
+                          id="email"
+                        />
+                      </Form.Item>
+                    ) : null}
+                  </div>
+                ) : (
                   <Input
-                    value={this.state.password}
+                    value={this.state.validationCode}
                     onChange={this.handleInputChange}
-                    name="password"
-                    placeholder="Password"
-                    id="password"
+                    name="validationCode"
+                    placeholder="Validation Code"
+                    id="validationCode"
                   />
-                </Form.Item>
-                {this.props.isNewUser ? <Form.Item type="text">
-                  <Input
-                    value={this.state.email}
-                    onChange={this.handleInputChange}
-                    name="email"
-                    placeholder="Email"
-                    id="email"
-                  />
-                </Form.Item> : null}
+                )}
+
                 <Button
                   type="primary"
                   htmlType="submit"
@@ -101,15 +145,36 @@ class Login extends Component {
                   loading={this.state.loading}
                   onClick={this.enterLoading}
                 >
-                  {this.props.isNewUser ? "Sign Up" : "Log In"}
+                  {!this.props.isPendingValidation
+                    ? this.props.isNewUser
+                      ? "Sign Up"
+                      : "Log In"
+                    : "Confirm Code"}
                 </Button>
               </Form>
             </Row>
             <Row>
-              <p className="modal-bottom">
-                New to EventPull?
-                <span className="signup-link" onClick={this.props.handleNewUser}>{this.props.isNewUser ? "Log In" : "Sign Up"}</span>
-              </p>
+              {!this.props.isPendingValidation ? (
+                <p className="modal-bottom">
+                  New to EventPull?
+                  <span
+                    className="signup-link"
+                    onClick={this.props.handleNewUser}
+                  >
+                    {this.props.isNewUser ? "Log In" : "Sign Up"}
+                  </span>
+                </p>
+              ) : (
+                <p className="modal-bottom">
+                  Can't find your code?
+                  <span
+                    className="signup-link"
+                    onClick={this.resendValidation}
+                  >
+                    Send Again
+                  </span>
+                </p>
+              )}
             </Row>
           </Col>
         </Row>
